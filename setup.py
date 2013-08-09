2f015b81751865792dfa7c3804eb01b707f3df46
Michael JasonSmith <mpj17@onlinegroups.net>
2013-08-09 11:57:53 -0700

    Updating the license and copyright.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -11,23 +11,23 @@ setup(name='gs.group.type.closed',
     long_description=open("README.txt").read() + "\n" +
                     open(os.path.join("docs", "HISTORY.txt")).read(),
     classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: Other/Proprietary License",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    keywords='user, group, page',
+    keywords='group closed',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
     url='http://groupserver.org',
-    license='ZPL',
+    license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.type',],
+    namespace_packages=['gs', 'gs.group', 'gs.group.type', ],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
@@ -42,4 +42,3 @@ setup(name='gs.group.type.closed',
     # -*- Entry points: -*-
     """,
 )
-
