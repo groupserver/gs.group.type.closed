2e5b903206a04ba6621b2d13d2803f5223e61d63
Michael JasonSmith <mpj17@onlinegroups.net>
2015-08-17 16:36:35 -0700

    Not sending the group status information about a closed group.

---
+++ gs/group/type/closed/sitegroups.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2015 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+from __future__ import absolute_import, print_function, unicode_literals
+from Products.GSGroup.groupInfo import GSGroupInfo
+
+
+class StatusGroupInfo(GSGroupInfo):
+    def __init__(self, userInfo, group):
+        self.userInfo = userInfo
+        super(StatusGroupInfo, self).__init__(group)
+
+    @property
+    def show(self):
+        '''Determine if the group should be shown to the person. (Always ``False``)'''
+        retval = False
+        return retval
